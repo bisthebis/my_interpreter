e9ab0ad6b80dae86e2df393430675ca2b3a4fd74
bisthebis <bism25@hotmail.com>
2017-01-22 15:14:04 +0100

    Basic structure for opening file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <QCoreApplication>
+#include <QtCore>
+
+QString getFileContent(const QString& path);
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QString path = QStandardPaths::standardLocations(QStandardPaths::DataLocation)[0] + "/sample.mt";
+    qDebug() << getFileContent(path);
 
     return a.exec();
+}
+
+QString getFileContent(const QString &path)
+{
+
+
+
+    return "";
 }
