5859a7f39ff598993bfc033dbc100b2e89468eeb
bisthebis <bism25@hotmail.com>
2017-01-23 20:06:13 +0100

    Made token mutable and default constructible (qvector requirement)

--- token.h
+++ token.h
@@ -12,18 +12,19 @@ public:
 
         LET, PRINT,
 
-        IDENTIFIER, NUMBER //An identifier starts by a letter or _ and an number is an integral
+        IDENTIFIER, NUMBER, //An identifier starts by a letter or _ and an number is an integral
+        INCORRECT_TOKEN
     };
 
 
 public:
-    Token(TokenType type, QString lexeme, QVariant value, int line);
+    Token(TokenType type = INCORRECT_TOKEN, QString lexeme = "Default constructed token. ERROR", QVariant value = "Error", int line = -1);
     QString toString() const;
 
-    const TokenType type;
-    const QString lexeme;
-    const QVariant value;
-    const int line;
+    TokenType type;
+    QString lexeme;
+    QVariant value;
+    int line;
 };
 
 #endif // TOKEN_H
