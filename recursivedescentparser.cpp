7c663b8073e3e6f5ad33857d07e7d9ead44e7fe9
bisthebis <bism25@hotmail.com>
2017-01-31 19:37:50 +0100

    Added utility for parser

--- recursivedescentparser.cpp
+++ recursivedescentparser.cpp
@@ -1,8 +1,24 @@
 #include "recursivedescentparser.h"
+#include <QString>
 
 namespace RecursiveDescent {
-RecursiveDescentParser::RecursiveDescentParser(const QVector<Token>& t) : tokens(t) {
+RecursiveDescentParser::RecursiveDescentParser(const QVector<Token>& t) : tokens(t), it(t.begin()), end(t.end()) {
 
+}
+
+bool RecursiveDescentParser::accept(Token::TokenType t) const
+{
+    if (it->type == t)
+    {
+        ++it;
+        return true;
+    }
+    return false;
+}
+void RecursiveDescentParser::expect(Token::TokenType t, QString err) const
+{
+    if (it->type != t)
+        throw MyException(QStringLiteral("Syntax Error : ") + err);
 }
 
 QSharedPointer<ASTNode> RecursiveDescentParser::parse() const {
--- recursivedescentparser.h
+++ recursivedescentparser.h
@@ -14,6 +14,10 @@ class RecursiveDescentParser
 {
 private:
     const QVector<Token>& tokens;
+    QVector<Token>::const_iterator it;
+    const QVector<Token>::const_iterator end;
+    bool accept(Token::TokenType t) const; //Checks whether the current token is a t, and advance it if it's true
+    void expect(Token::TokenType t, QString err) const; //Checks if current token is a t, and throw Exception if it's not
 public:
     RecursiveDescentParser(const QVector<Token>& t);
     QSharedPointer<ASTNode> parse() const;
