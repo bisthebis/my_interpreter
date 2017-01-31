f66dd77d177e0e538329aeba9d7f5da684685ae6
bisthebis <bism25@hotmail.com>
2017-01-31 19:31:29 +0100

    Added ASTNodes constructors

--- astnode.h
+++ astnode.h
@@ -2,6 +2,9 @@
 #define ASTNODE_H
 
 #include "astvisitor.h"
+#include <QVector>
+#include <QString>
+#include <QSharedPointer>
 
 namespace RecursiveDescent {
 
@@ -13,38 +16,56 @@ struct ASTNode {
 
 struct ASTProgram : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitProgram(*this);}
+    QVector<QSharedPointer<ASTNode>> instructions;
 };
 
 struct ASTLetStatement : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitLetStatement(*this);}
+    ASTLetStatement(QString v, QSharedPointer<ASTNode> e) : varName(v), expression(e){}
+    QString varName;
+    QSharedPointer<ASTNode> expression;
 };
 
 struct ASTPrintStatement : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitPrintStatement(*this);}
+    ASTPrintStatement(QString v) : varName(v) {}
+    QString varName;
 };
 
 struct ASTPlus : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitPlus(*this);}
+    QSharedPointer<ASTNode> lhs, rhs;
+    ASTPlus(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
 };
 
 struct ASTMinus : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitMinus(*this);}
+    QSharedPointer<ASTNode> lhs, rhs;
+    ASTMinus(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
 };
 
 struct ASTTimes : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitTimes(*this);}
+    QSharedPointer<ASTNode> lhs, rhs;
+    ASTTimes(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
 };
 
 struct ASTSlash : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitSlash(*this);}
+    QSharedPointer<ASTNode> lhs, rhs;
+    ASTSlash(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
 };
 
 struct ASTVariable : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitVariable(*this);}
+    ASTVariable(QString v) : varName(v) {}
+    QString varName;
 };
 
 struct ASTNumber : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitNumber(*this);}
+    const double value;
+    ASTNumber (double v) : value(v) {}
 };
 
 }
