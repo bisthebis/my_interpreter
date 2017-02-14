58c756504ae45cb5d3bdb6b696c4063ff501af62
bisthebis <bism25@hotmail.com>
2017-02-14 19:12:49 +0100

    cleaning with type alias

--- astnode.h
+++ astnode.h
@@ -8,23 +8,24 @@
 
 namespace RecursiveDescent {
 
-
 struct ASTNode {
     virtual void accept(ASTVisitor& v) = 0;
     virtual ~ASTNode(){}
 };
 
+using Node = QSharedPointer<ASTNode>;
+
 struct ASTProgram : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitProgram(*this);}
-    ASTProgram(QVector<QSharedPointer<ASTNode>> data) : instructions(data){}
-    QVector<QSharedPointer<ASTNode>> instructions;
+    ASTProgram(QVector<Node> data) : instructions(data){}
+    QVector<Node> instructions;
 };
 
 struct ASTLetStatement : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitLetStatement(*this);}
-    ASTLetStatement(QString v, QSharedPointer<ASTNode> e) : varName(v), expression(e){}
+    ASTLetStatement(QString v, Node e) : varName(v), expression(e){}
     QString varName;
-    QSharedPointer<ASTNode> expression;
+    Node expression;
 };
 
 struct ASTPrintStatement : ASTNode {
@@ -35,32 +36,32 @@ struct ASTPrintStatement : ASTNode {
 
 struct ASTPlus : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitPlus(*this);}
-    QSharedPointer<ASTNode> lhs, rhs;
-    ASTPlus(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
+    Node lhs, rhs;
+    ASTPlus(Node l, Node r) : lhs(l), rhs(r) {}
 };
 
 struct ASTMinus : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitMinus(*this);}
-    QSharedPointer<ASTNode> lhs, rhs;
-    ASTMinus(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
+    Node lhs, rhs;
+    ASTMinus(Node l, Node r) : lhs(l), rhs(r) {}
 };
 
 struct ASTTimes : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitTimes(*this);}
-    QSharedPointer<ASTNode> lhs, rhs;
-    ASTTimes(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
+    Node lhs, rhs;
+    ASTTimes(Node l, Node r) : lhs(l), rhs(r) {}
 };
 
 struct ASTSlash : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitSlash(*this);}
-    QSharedPointer<ASTNode> lhs, rhs;
-    ASTSlash(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
+    Node lhs, rhs;
+    ASTSlash(Node l, Node r) : lhs(l), rhs(r) {}
 };
 
 struct ASTExponent : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitExponent(*this);}
-    QSharedPointer<ASTNode> lhs, rhs;
-    ASTExponent(QSharedPointer<ASTNode> l, QSharedPointer<ASTNode> r) : lhs(l), rhs(r) {}
+    Node lhs, rhs;
+    ASTExponent(Node l, Node r) : lhs(l), rhs(r) {}
 };
 
 struct ASTVariable : ASTNode {
@@ -77,16 +78,16 @@ struct ASTNumber : ASTNode {
 
 struct ASTCond : ASTNode {
     virtual void accept(ASTVisitor& v) {v.visitCond(*this);}
-    QSharedPointer<ASTNode> cond, then, otherwise;
-    ASTCond(QSharedPointer<ASTNode> c, QSharedPointer<ASTNode> t, QSharedPointer<ASTNode> o) : cond(c), then(t), otherwise(o){}
+    Node cond, then, otherwise;
+    ASTCond(Node c, Node t, Node o) : cond(c), then(t), otherwise(o){}
 };
 
 struct ASTComparison : ASTNode {
     enum Comparator {EQ, NEQ, GT, GET, LT, LET};
-    QSharedPointer<ASTNode> lhs, rhs;
+    Node lhs, rhs;
     Comparator op;
     virtual void accept(ASTVisitor& v) {v.visitComparison(*this);}
-    ASTComparison(QSharedPointer<ASTNode> lhs, QSharedPointer<ASTNode> rhs, Comparator comp) : lhs(lhs), rhs(rhs), op(comp) {}
+    ASTComparison(Node lhs, Node rhs, Comparator comp) : lhs(lhs), rhs(rhs), op(comp) {}
 };
 
 }
